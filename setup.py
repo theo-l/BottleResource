31b76d9ed070017af53c944532a3ff8e957061b3
theo-l <theol.liang@gmail.com>
2019-08-01 02:37:07 -0300

    change author name

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=['.'],
     url='https://github.com/theo-l/BottleResource',
     license='',
-    author='liang',
+    author='Liang Guisheng',
     author_email='theol.liang@foxmail.com',
     description='Class based bottle endpoint resource plugin',
     include_package_data=True
